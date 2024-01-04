79903b7d731b321f41e6cf61b2be7011e4d58854
scarlett <2365112062@qq.com>
2024-01-04 18:43:38 +0800

    MPC with no perception,inputs are thrust and torque

--- model/quadrotor_nmpc.cpp
+++ model/quadrotor_nmpc.cpp
@@ -1,110 +1,170 @@
-
+#include <memory>
+#include <acado_optimal_control.hpp>
 #include <acado_code_generation.hpp>
+#include <acado/matrix_vector/matrix_vector.hpp>
 
 USING_NAMESPACE_ACADO
-
 int main(int argc, char * const argv[ ])
 {
-	//
-	// Variables
-	//
+  /*
+  Switch between code generation and analysis.
 
-	DifferentialState	x, y, w, dx, dy, dw;
-	AlgebraicState		mu;
-	Control				F;
+  If CODE_GEN is true the system is compiled into an optimizaiton problem
+  for real-time iteration and all code to run it online is generated.
+  Constraints and reference structure is used but the values will be set on
+  runtinme.
 
-	IntermediateState	c, dc;
+  If CODE_GEN is false, the system is compiled into a standalone optimization
+  and solved on execution. The reference and constraints must be set in here.
+  */
+  const bool CODE_GEN = true;
 
-	const double		m = 1.0;
-	const double		mc = 1.0;
-	const double		L = 1.0;
-	const double		g = 9.81;
-	const double		p = 5.0;
+  // System variables, x
+  DifferentialState		p_x, p_y, p_z;//position, in world frame
+  DifferentialState		phi, theta, psi;//attitude
+  DifferentialState		v_x, v_y, v_z;//velocity, in world frame
+  DifferentialState		w_x, w_y, w_z;//angular velocity, in body frame
+//   DifferentialState		f1, f2, f3, f4;//the force produced by the individual propeller
+//   Control				f1_dot, f2_dot, f3_dot, f4_dot;
+  //先把基础的功能做出来再优化吧
+  Control				thrust, tau_x, tau_y, tau_z;//in body frame
+  DifferentialEquation  f;//state equation
+  Function              h, hN;
 
-	c	= 0.5 * ((x - w) * (x - w) + y * y - L * L);
-	dc	= dy * y + (dw - dx) * (w - x);
+  // Parameters with exemplary values. These are set/overwritten at runtime.
+  const double t_start = 0.0;     // Initial time [s]
+  const double t_end = 2.0;       // Time horizon [s]
+  const double dt = 0.1;          // Discretization time [s]
+  const int N = round(t_end/dt);  // Number of nodes
+  const double g_z = 9.8066;      // Gravity is everywhere [m/s^2]
+  const double mass=1.5;//运行时能否改变这些数据，例程写的是可以
+  const double Jxx = 1;
+  const double Jyy = 1;
+  const double Jzz = 1;
+  const double thrust_min = 0;         // Minimal thrust [N]
+  const double thrust_max = 25;        // Maximal thrust [N]
+  const double taux_min = 0;         // Minimal taux [Nm]
+  const double taux_max = 0.5;       // Maximal taux [Nm]
+  const double tauy_min = 0;         // Minimal tauy [Nm]
+  const double tauy_max = 0.5;       // Maximal tauy [Nm]
+  const double tauz_min = 0;         // Minimal tauz [Nm]
+  const double tauz_max = 0.5;       // Maximal tauz [Nm]
 
-	//
-	// Differential algebraic equation
-	//
+  f << dot(p_x) ==  v_x;
+  f << dot(p_y) ==  v_y;
+  f << dot(p_z) ==  v_z;
 
-	DifferentialEquation f;
+  f << dot(phi) ==   w_x * 1 + w_y * sin(phi)*tan(theta) + w_z * cos(phi)*tan(theta);
+  f << dot(theta) == w_x * 0 + w_y * cos(phi)            + w_z * (-sin(phi));
+  f << dot(psi) ==   w_x * 0 + w_y*sin(phi)/cos(theta)   + w_z * cos(phi)/cos(theta); 
 
-	f << 0 == dot( x ) - dx;
-	f << 0 == dot( y ) - dy;
-	f << 0 == dot( w ) - dw;
+  f << dot(v_x) == (cos(psi)*sin(theta)*cos(phi)+sin(psi)*sin(phi))*(thrust)/mass;
+  f << dot(v_y) == (sin(psi)*sin(theta)*cos(phi)-cos(psi)*sin(phi))*(thrust)/mass;
+  f << dot(v_z) == (cos(theta)*cos(phi))*(thrust)/mass - g_z;
 
-	f << 0 == m * dot( dx ) + (x - w) * mu;
-	f << 0 == m * dot( dy ) + y * mu + m * g;
-	f << 0 == mc * dot( dw ) + (w - x) * mu  - F;
+  f << dot(w_x) == (tau_x - (Jzz*w_z*w_y - Jyy*w_y*w_z))/Jxx;
+  f << dot(w_y) == (tau_y - (Jxx*w_x*w_z - Jzz*w_z*w_x))/Jyy;
+  f << dot(w_z) == (tau_z - (Jyy*w_x*w_y - Jxx*w_x*w_y))/Jzz;
 
-	f << 0 == (x - w) * dot( dx ) + y * dot( dy ) + (w - x) * dot( dw )
-					- (-p * p * c - 2 * p * dc - dy * dy - (dw - dx) * (dw - dx));
+  // 这里h是不是应该是误差啊，为啥输入的是state呢
+  // Cost: Sum(i=0, ..., N-1){h_i' * Q * h_i} + h_N' * Q_N * h_N
+  // Running cost vector consists of all states and inputs.
+  h << p_x << p_y << p_z
+	<< phi << theta << psi
+	<< v_x << v_y << v_z
+    << w_x << w_y << w_z
+	<< thrust << tau_x << tau_y << tau_z;
+  // End cost vector consists of all states (no inputs at last state).
 
-	//
-	// Weighting matrices and reference functions
-	//
+  hN << p_x << p_y << p_z
+	<< phi << theta << psi
+	<< v_x << v_y << v_z
+	<< w_x << w_y << w_z;
 
-	Function rf;
-	Function rfN;
+  DMatrix Q(h.getDim(), h.getDim());
+  Q.setIdentity();
+  Q(0,0) = 100;   // p_x
+  Q(1,1) = 100;   // p_y
+  Q(2,2) = 100;   // p_z
+  Q(3,3) = 10;   // phi
+  Q(4,4) = 10;   // theta
+  Q(5,5) = 10;   // psi
+  Q(6,6) = 10;   // v_x
+  Q(7,7) = 10;   // v_y
+  Q(8,8) = 10;   // v_z
+  Q(9,9) = 10;   // w_x
+  Q(10,10) = 10;  // w_y
+  Q(11,11) = 10;  // w_z
+  Q(12,12) = 1;   // thrust
+  Q(13,13) = 1;   // tau_x
+  Q(14,14) = 1;   // tau_y
+  Q(15,15) = 1;   // tau_z
 
-	rf << x << y << w << dx << dy << dw << F;
-	rfN << x << y << w << dx << dy << dw;
+  DMatrix QN(h.getDim(), h.getDim());
+  QN.setIdentity();
+  QN(0,0) = 100;   // p_x
+  QN(1,1) = 100;   // p_y
+  QN(2,2) = 100;   // p_z
+  QN(3,3) = 10;   // phi
+  QN(4,4) = 10;   // theta
+  QN(5,5) = 10;   // psi
+  QN(6,6) = 10;   // v_x
+  QN(7,7) = 10;   // v_y
+  QN(8,8) = 10;   // v_z
+  QN(9,9) = 10;   // w_x
+  QN(10,10) = 10;  // w_y
+  QN(11,11) = 10;  // w_z
 
-	DMatrix W = eye<double>( rf.getDim() );
-	DMatrix WN = eye<double>( rfN.getDim() ) * 10;
+  //
+  // Optimal Control Problem
+  //
+  OCP ocp( t_start, t_end, N );
+  
+  // For code generation, references are set during run time.
+  BMatrix Q_sparse(h.getDim(), h.getDim());
+  Q_sparse.setIdentity();
+  BMatrix QN_sparse(hN.getDim(), hN.getDim());
+  QN_sparse.setIdentity();
+  ocp.minimizeLSQ( Q_sparse, h);
+  ocp.minimizeLSQEndTerm( QN_sparse, hN );
 
-	//
-	// Optimal Control Problem
-	//
+  // Add system dynamics
+  ocp.subjectTo( f );
 
-	const int N  = 10;
-	const int Ni = 4;
-	const double Ts = 0.1;
+  // Add constraints
+  ocp.subjectTo(thrust_min <= thrust <= thrust_max);
+  ocp.subjectTo(taux_min <= tau_x <= taux_max);
+  ocp.subjectTo(tauy_min <= tau_y <= tauy_max);
+  ocp.subjectTo(tauz_min <= tau_z <= tauz_max);
 
-	OCP ocp(0, N * Ts, N);
+  ocp.setNOD(10);
+  //
+  // Export the code:
+  //
+  OCPexport mpc( ocp );
 
-	ocp.subjectTo( f );
+  mpc.set(HESSIAN_APPROXIMATION, GAUSS_NEWTON);
+  mpc.set(DISCRETIZATION_TYPE, MULTIPLE_SHOOTING);
+  mpc.set(SPARSE_QP_SOLUTION,     FULL_CONDENSING_N2);  // due to qpOASES
+  mpc.set(INTEGRATOR_TYPE, INT_IRK_GL4);
+  mpc.set(NUM_INTEGRATOR_STEPS, N);
+  mpc.set(QP_SOLVER, QP_QPOASES);
+  mpc.set(HOTSTART_QP, YES);
+  mpc.set(CG_USE_OPENMP,                    YES);       // paralellization
+  mpc.set(CG_HARDCODE_CONSTRAINT_VALUES,    NO);        // set on runtime
+  mpc.set(CG_USE_VARIABLE_WEIGHTING_MATRIX, YES);       // time-varying costs
+  mpc.set( USE_SINGLE_PRECISION,        YES);           // Single precision
+  
+  // Do not generate tests, makes or matlab-related interfaces.
+  mpc.set(GENERATE_TEST_FILE, NO);
+  mpc.set(GENERATE_MAKE_FILE, NO);
+  mpc.set(GENERATE_MATLAB_INTERFACE, NO);
+  mpc.set( GENERATE_SIMULINK_INTERFACE, NO);
 
-	ocp.minimizeLSQ(W, rf);
-	ocp.minimizeLSQEndTerm(WN, rfN);
-
-	ocp.subjectTo(-20 <= F <= 20);
-//	ocp.subjectTo( -5 <= x <= 5 );
-
-	//
-	// Export the code:
-	//
-	OCPexport mpc( ocp );
-
-	mpc.set(HESSIAN_APPROXIMATION, GAUSS_NEWTON);
-	mpc.set(DISCRETIZATION_TYPE, MULTIPLE_SHOOTING);
-
-	mpc.set(INTEGRATOR_TYPE, INT_IRK_RIIA3);
-	mpc.set(NUM_INTEGRATOR_STEPS, N * Ni);
-
-	mpc.set(SPARSE_QP_SOLUTION, FULL_CONDENSING);
-//	mpc.set(SPARSE_QP_SOLUTION, CONDENSING);
-	mpc.set(QP_SOLVER, QP_QPOASES);
-//	mpc.set(MAX_NUM_QP_ITERATIONS, 20);
-	mpc.set(HOTSTART_QP, YES);
-
-//	mpc.set(SPARSE_QP_SOLUTION, SPARSE_SOLVER);
-//	mpc.set(QP_SOLVER, QP_FORCES);
-
-//	mpc.set(LEVENBERG_MARQUARDT, 1.0e-10);
-
-	mpc.set(GENERATE_TEST_FILE, NO);
-	mpc.set(GENERATE_MAKE_FILE, NO);
-	mpc.set(GENERATE_MATLAB_INTERFACE, YES);
-
-//	mpc.set(USE_SINGLE_PRECISION, YES);
-//	mpc.set(CG_USE_OPENMP, YES);
-
-	if (mpc.exportCode( "quadrotor_nmpc" ) != SUCCESSFUL_RETURN)
-		exit( EXIT_FAILURE );
-
-	mpc.printDimensionsQP( );
-
-	return EXIT_SUCCESS;
+  // Finally, export everything.
+  if (mpc.exportCode( "quadrotor_nmpc" ) != SUCCESSFUL_RETURN)
+    exit( EXIT_FAILURE );
+  mpc.printDimensionsQP( );
+  
+  return EXIT_SUCCESS;
 }
