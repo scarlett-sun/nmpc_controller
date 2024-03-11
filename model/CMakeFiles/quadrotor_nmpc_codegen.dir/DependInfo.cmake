135bfa2cd58d5e729eef1cd1e90f6a30d1819a50
scarlett <2365112062@qq.com>
2024-03-11 21:36:01 +0800

    Add the generated model for default use

---
+++ model/CMakeFiles/quadrotor_nmpc_codegen.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/scarlett/Workspaces/rotors_integration_ws/src/perception_nmpc/nmpc_controller/model/quadrotor_nmpc.cpp" "/home/scarlett/Workspaces/rotors_integration_ws/src/perception_nmpc/nmpc_controller/model/CMakeFiles/quadrotor_nmpc_codegen.dir/quadrotor_nmpc.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "."
+  "/home/scarlett/Workspaces/ACADOtoolkit"
+  "/home/scarlett/Workspaces/ACADOtoolkit/acado"
+  "/home/scarlett/Workspaces/ACADOtoolkit/external_packages"
+  "/home/scarlett/Workspaces/ACADOtoolkit/external_packages/qpOASES-3.2.0/include"
+  "/home/scarlett/Workspaces/ACADOtoolkit/build"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ model/CMakeFiles/quadrotor_nmpc_codegen.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/quadrotor_nmpc_codegen.dir/quadrotor_nmpc.cpp.o"
+  "quadrotor_nmpc_codegen"
+  "quadrotor_nmpc_codegen.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/quadrotor_nmpc_codegen.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
